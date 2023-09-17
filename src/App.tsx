783937cc89c7184a6e2927348e7c425076f02697
egg-silver <v6984@naver.com>
2023-09-17 23:47:43 +0900

    feat/#8 로그인 회원가입 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
 import MapContainer from './utils/Map';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <MapContainer/>
+      {/* <MapContainer/> */}
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/singUp" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
