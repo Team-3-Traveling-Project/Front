d726149e7f20d23ffc2ad74994f21db7623b5799
egg-silver <v6984@naver.com>
2023-09-21 11:48:21 +0900

    일정 조회 페이지 axios 연결

--- src/components/ScheduleBox.tsx
+++ src/components/ScheduleBox.tsx
@@ -3,28 +3,30 @@ import styled from 'styled-components';
 
 type ScheduleBoxProps = {
   imgUrl: string;
-  num: string;
+  num: any;
   name: string;
   category: string;
   location: string;
   onClick: () => void;
+  x?: string;
+  y?: string;
 };
 
-export default function ScheduleBox({ imgUrl, num, name, category, location, onClick }: ScheduleBoxProps) {
+export default function ScheduleBox({ imgUrl, num, name, category, location, onClick, x, y }: ScheduleBoxProps) {
   return (
     <>
       <Box onClick={onClick}>
-        <Num>1</Num>
+        <Num>{num}</Num>
         <TextBox>
           <span style={{ color: '#63BEC6', marginRight: '4px' }}>명소</span>
-          <Title>경복궁</Title>
+          <Title>{location}</Title>
         </TextBox>
         <img
           style={{
             width: '55px',
             height: '55px',
             borderRadius: '6px',
-            marginLeft: '90px',
+            marginLeft:'10px'
           }}
           src="https://cdn.myro.co.kr/prod/image/city/Tokyo.jpg"
           alt="Travel"
@@ -35,10 +37,20 @@ export default function ScheduleBox({ imgUrl, num, name, category, location, onC
 }
 
 const Box = styled.button`
-  margin: 20px;
-  width: 238px;
-  height: 56px;
+  width: 330px;
+  height: 62px;
+  padding: 4px;
+  margin: 8px;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 8px;
+  box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.158);
+  transition: all 0.3s ease-out;
+  &:hover {
+    box-shadow: 3px 4px 9px rgba(0, 0, 0, 0.247); /* 마우스 호버 시 그림자 증가 */
+  }
 `;
 const Num = styled.div`
   width: 20px;
@@ -48,6 +60,7 @@ const Num = styled.div`
   border-radius: 50%;
   line-height: 18px;
   text-align: center;
+  margin-right: 8px;
 `;
 
 const TextBox = styled.div`
