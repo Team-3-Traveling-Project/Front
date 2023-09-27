0d641c7d18b7cda6e67d426791a5298992e4bbe8
wynter24 <m.wynter.k@gmail.com>
2023-09-27 17:59:08 +0900

    소셜로그인 수정

--- src/pages/Redirection.tsx
+++ src/pages/Redirection.tsx
@@ -14,9 +14,11 @@ const Redirection = () => {
         console.log(response);
 
         // 토큰을 받아서 localStorage 같은 곳에 저장하는 코드를 여기에 쓴다.
-        localStorage.setItem('Authorization', response.headers.authorization); // 일단 이름만 저장했다.
-
+        if (response.data.statusCode === 200) {
+        localStorage.setItem('Authorization', response.headers.authorization);
+        // checkUser();
         navigate('/');
+      }
       } catch (error) {
         console.log('kakao 소셜 로그인 에러 : ', error);
         window.alert('소셜 로그인에 실패하였습니다.');
